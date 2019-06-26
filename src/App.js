6f73ce408998b72bd6d4489bfe51b905f6836ea5
samidahlawi <o.t5@hotmail.com>
2019-06-26 20:36:53 +0300

    add files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Header"
+
+function App() {
+  return (
+    <div >
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import logo from './logo.svg';
+import './App.css';
+
+const Header = () => {
+    return(
+      <header>
+      <h1>Hello World!!</h1>
+      <h2>Goodbye Moon</h2>
+    </header>
+    )
+  }
+
+
+  export default Header;
